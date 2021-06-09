a8ed725ad1bbc5c732eec354d2d4086742600527
LAKKI <thakurluckysinghbrh@gmail.com>
2021-06-09 13:18:47 +0530

    readme updated

--- src/index.ts
+++ src/index.ts
@@ -8,15 +8,14 @@ joplin.plugins.register({
 			label: 'open this note on each startup',
 			iconName: 'fas fa-home',
 			execute: () => new Promise((resolve, reject) => {
-				const selectedNote=joplin.workspace.selectedNote();
-				selectedNote.then(currentNote=>localStorage.setItem('homeNoteId',currentNote.id)).then(d=>alert('Current note selected as homenote'))
+				const selectedNote = joplin.workspace.selectedNote();
+				selectedNote.then(currentNote => localStorage.setItem('homeNoteId', currentNote.id)).then(d => alert('Current note selected as homenote'))
 				resolve(1);
-				
 			})
 		})
 		joplin.views.toolbarButtons.create('idHomenote', 'setHomenote', ToolbarButtonLocation.EditorToolbar);
 
-		const homeNoteId=localStorage.getItem('homeNoteId');
-		if(homeNoteId) {joplin.commands.execute('openNote',homeNoteId)}
+		const homeNoteId = localStorage.getItem('homeNoteId');
+		if (homeNoteId) { joplin.commands.execute('openNote', homeNoteId) }
 	},
 });
