7be6cd51eb7047e333e13b8d694edc0451fd84b2
LAKKI <thakurluckysinghbrh@gmail.com>
2021-07-06 07:06:29 +0530

    auto formatted

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,32 @@
-import joplin from 'api';
-import { ToolbarButtonLocation } from 'api/types';
+import joplin from "api";
+import { ToolbarButtonLocation } from "api/types";
 
 joplin.plugins.register({
-	onStart: async function () {
-		const homeNoteId = localStorage.getItem('homeNoteId');
-		if (homeNoteId) { joplin.commands.execute('openNote', homeNoteId) }
+  onStart: async function () {
+    const homeNoteId = localStorage.getItem("homeNoteId");
+    if (homeNoteId) {
+      joplin.commands.execute("openNote", homeNoteId);
+    }
 
-		joplin.commands.register({
-			name: 'setHomenote',
-			label: 'open this note on each startup',
-			iconName: 'fas fa-home',
-			execute: () => new Promise((resolve, reject) => {
-				const selectedNote = joplin.workspace.selectedNote();
-				selectedNote.then(currentNote => localStorage.setItem('homeNoteId', currentNote.id)).then(d => alert('Current note selected as homenote'))
-				resolve(1);
-			})
-		})
-		joplin.views.toolbarButtons.create('idHomenote', 'setHomenote', ToolbarButtonLocation.EditorToolbar);
-			
-	},
+    joplin.commands.register({
+      name: "setHomenote",
+      label: "open this note on each startup",
+      iconName: "fas fa-home",
+      execute: () =>
+        new Promise((resolve, reject) => {
+          const selectedNote = joplin.workspace.selectedNote();
+          selectedNote
+            .then((currentNote) =>
+              localStorage.setItem("homeNoteId", currentNote.id)
+            )
+            .then((d) => alert("Current note selected as homenote"));
+          resolve(1);
+        }),
+    });
+    joplin.views.toolbarButtons.create(
+      "idHomenote",
+      "setHomenote",
+      ToolbarButtonLocation.EditorToolbar
+    );
+  },
 });
