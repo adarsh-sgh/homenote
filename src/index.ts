f5a77cdec5568a63852d971e6215561e8f58602b
Adarsh Singh <thakurluckysinghbrh@gmail.com>
2021-10-30 12:08:46 +0530

    fix: avoid plugin crash when homenote is deleted

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,17 @@ joplin.plugins.register({
   onStart: async function () {
     const homeNoteId = localStorage.getItem("homeNoteId");
     if (homeNoteId) {
-      await joplin.commands.execute("openNote", homeNoteId);
+      try {
+				await joplin.commands.execute("openNote", homeNoteId);
+			} catch (error) {
+				//when note is deleted maybe
+				console.error(error.message);
+			}
     }
 
     await joplin.commands.register({
       name: "setHomenote",
-      label: "open this note on each startup",
+      label: "open this note each time joplin is launched",
       iconName: "fas fa-home",
       execute: () =>
         new Promise((resolve, reject) => {
