9a3f36717af2ab36c132ac7a2a70b2b11a6bb47d
Adarsh Singh <thakurluckysinghbrh@gmail.com>
2022-01-27 17:35:10 +0530

    Added commands and keyboard shortcuts

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,73 @@
 import joplin from "api";
-import { ToolbarButtonLocation } from "api/types";
+import { MenuItemLocation, ToolbarButtonLocation } from "api/types";
 
 joplin.plugins.register({
-  onStart: async function () {
-    const homeNoteId = localStorage.getItem("homeNoteId");
-    if (homeNoteId) {
-      try {
-				await joplin.commands.execute("openNote", homeNoteId);
-			} catch (error) {
-				//when note is deleted maybe
-				console.error(error.message);
-			}
-    }
+	onStart: async function () {
+		await joplin.commands.register({
+			name: "setHomenote",
+			label: "Set as Homenote",
+			iconName: "fas fa-home",
+			execute: () =>
+				new Promise((resolve, reject) => {
+					const selectedNote = joplin.workspace.selectedNote();
+					selectedNote
+						.then(currentNote =>
+							localStorage.setItem("homeNoteId", currentNote.id)
+						)
+						.then(async d => {
+							joplin.views.dialogs.open(regiteredHomenoteDialog);
+						});
+					resolve(1);
+				}),
+		});
+		await joplin.commands.register({
+			name: "openHomenote",
+			label: "Open the Homenote",
+			iconName: "fas fa-home",
+			execute: async () => {
+				const homeNoteId = localStorage.getItem("homeNoteId");
 
-    await joplin.commands.register({
-      name: "setHomenote",
-      label: "open this note each time joplin is launched",
-      iconName: "fas fa-home",
-      execute: () =>
-        new Promise((resolve, reject) => {
-          const selectedNote = joplin.workspace.selectedNote();
-          selectedNote
-            .then((currentNote) =>
-              localStorage.setItem("homeNoteId", currentNote.id)
-            )
-            .then((d) => alert("Current note selected as homenote"));
-          resolve(1);
-        }),
-    });
-    await joplin.views.toolbarButtons.create(
-      "idHomenote",
-      "setHomenote",
-      ToolbarButtonLocation.EditorToolbar
-    );
-  },
+				try {
+					await joplin.commands.execute("openNote", homeNoteId);
+				} catch (error) {
+					//when note is deleted maybe or no homenote set yet
+					console.error(error.message);
+				}
+			},
+		});
+
+		if (localStorage.getItem("homeNoteId"))
+			await joplin.commands.execute("openHomenote");
+		await joplin.views.toolbarButtons.create(
+			"idHomenote",
+			"setHomenote",
+			ToolbarButtonLocation.EditorToolbar
+		);
+
+		const regiteredHomenoteDialog = await joplin.views.dialogs.create(
+			"homenoteRegistered"
+		);
+		await joplin.views.dialogs.setHtml(
+			regiteredHomenoteDialog,
+			`<p>Current note selected as Homenote</p>`
+		);
+		await joplin.views.dialogs.setButtons(regiteredHomenoteDialog, [
+			{
+				id: "ok",
+			},
+		]);
+
+		await joplin.views.menuItems.create(
+			"setHomeNoteMenu",
+			"setHomenote",
+			MenuItemLocation.Tools,
+			{ accelerator: "CmdOrCtrl+Alt+Shift+H" }
+		);
+		await joplin.views.menuItems.create(
+			"openHomenoteMenu",
+			"openHomenote",
+			MenuItemLocation.Tools,
+			{ accelerator: "CmdOrCtrl+Shift+H" }
+		);
+	},
 });
